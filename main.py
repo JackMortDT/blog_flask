5cefb1806acdc7c6bbe838af0b2291f398ec9f30
LuisSas <luis@makingdevs.com>
2018-12-28 00:38:51 -0600

    Adding class Comment and tag with relations

--- main.py
+++ main.py
@@ -1,12 +1,71 @@
+import datetime
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255), nullable=False, index=True, unique=True)
+    password = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    tags = db.relationship('Tag', secondary=tags, backref='post', lazy='dynamic')
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.name)
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
+
 @app.route('/')
 def home():
-    return '<h1>Hello World!</h1>'
+    result = '<h1>Tables</h1><br><ul>'
+    for table in db.metadata.tables.items():
+        result += "<li>%s</li>" % str(table)
+    result += "</ul>"
+    return result
 
 if __name__ == '__main__':
     app.run()
+
