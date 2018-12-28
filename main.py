5923d34d07edcd1b04053dded52882e5bc85d99b
LuisSas <luis@makingdevs.com>
2018-12-28 00:52:46 -0600

    Adding post_tags table and other rows on comment table

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ app.config.from_object(DevConfig)
 
 db = SQLAlchemy(app)
 
+tags = db.Table('post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(255), nullable=False, index=True, unique=True)
@@ -29,6 +34,7 @@ class Post(db.Model):
     publish_date = db.Column(db.DateTime(), default=datetime.datetime.now)
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
     user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    tags = db.relationship('Tag', secondary=tags, backref=db.backref('post', lazy='dynamic'))
 
     def __init__(self, title):
         self.title = title
@@ -40,7 +46,8 @@ class Comment(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(255))
     text = db.Column(db.Text())
-    tags = db.relationship('Tag', secondary=tags, backref='post', lazy='dynamic')
+    date= db.Column(db.DateTime())
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
 
     def __init__(self, name):
         self.name = name
