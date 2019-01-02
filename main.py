3d74da6145da1315611ef779fca3f4c0f8de0496
LuisSas <luis@makingdevs.com>
2019-01-02 00:56:39 -0600

    Adding some routes for get all post and post by different filters

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import DevConfig
+from sqlalchemy import func
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -67,13 +68,58 @@ class Tag(db.Model):
     def __repr__(self):
         return "<Tag '{}'>".format(self.title)
 
+def sidebar_data():
+    recent = Post.query.order_by(
+        Post.publish_date.desc()
+    ).limit(5).all()
+    top_tags = db.session.query(
+        Tag, func.count(tags.c.post_id).label('total')
+    ).join(
+        tags
+    ).group_by(Tag).order_by('total DESC').limit(5).all()
+    return recent, top_tags
+
 @app.route('/')
-def home():
-    result = '<h1>Tables</h1><br><ul>'
-    for table in db.metadata.tables.items():
-        result += "<li>%s</li>" % str(table)
-    result += "</ul>"
-    return result
+@app.route('/<int:page>')
+def home(page=1):
+    posts = Post.query.order_by(Post.publish_date.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    recent, top_tags = sidebar_data()
+    return render_template(
+        'home.html',
+        posts=posts,
+        recent=recent,
+        top_tags=top_tags
+    )
+
+@app.route('/post/<int:post:id>')
+def post(post_id)
+    post = Post.query.get_or_404(post_id)
+
+@app.route('/post_by_tag/<string:tag_name>')
+def post_by_tag(tag_name):
+    tag = Tag.query.filter_by(title=tag_name).first_or_404()
+    posts = tag.posts.order_by(Post.publish_date.desc()).all()
+    recent, top_tags = sidebar_data()
+    return render_template(
+        'tag.html',
+        tag=tag,
+        posts=posts,
+        recent=recent,
+        top_tags=top_tags
+    )
+
+@app.route('/post_by_user/<string:username>')
+def post_by_user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = user.posts.order_by(Post.publish_date.desc()).all()
+    recent, top_tags = sidebar_data()
+    return render_template(
+        'user.html',
+        user=user,
+        posts=posts,
+        recent=recent,
+        top_tags=top_tags
+    )
 
 if __name__ == '__main__':
     app.run()
